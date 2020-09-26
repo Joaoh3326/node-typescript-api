9c512e936bbb50c084e3fea1231cd6bbffd0249d
joaoh3326 <joaoh3326@gmail.com>
2020-09-25 21:32:46 -0300

    finalizando o setup de test e criando o primeiro test

---
+++ test/functional/forecast.test.ts
@@ -0,0 +1,9 @@
+import { stat } from 'fs/promises';
+import supertest from 'supertest'
+describe('Beach forecast functional test', () => {
+  it('should return a forecast with just a few times', async () => {
+    const { body, status } = await global.testRequest.get('/forecast')
+    expect(status).toBe(200);
+    expect(body).toEqual([])
+  })
+});
